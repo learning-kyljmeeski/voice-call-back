8e4bc3cfe4936a5352df92e483b4e018d9a864e8
learning-kyljmeeski <learning.kyljmeeski@gmail.com>
2024-05-15 23:14:35 +0600

    init

---
+++ src/test/java/com/kyljmeeski/voicecallapplication/VoiceCallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyljmeeski.voicecallapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoiceCallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
